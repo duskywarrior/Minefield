00c80d08090c093735e5640c9ad45aab3632fd30
gethinll <54894063+gethinll@users.noreply.github.com>
2019-10-10 21:52:50 +0100

    Bug Fixes
    the old copy wouldnt allow any chnages with object becoming invisible, any chance of reverting this failed therefore i am reverting back to a working state

--- PROJECT/Minefield/Form1.cs
+++ PROJECT/Minefield/Form1.cs
@@ -1,20 +1,4 @@
-﻿/******************************************************************************
-* All rights reserved. No part of this publication may be reproduced, stored
-* in a retrieval system, or transmitted in any form or by any means,
-* electronic, mechanical, photocopying, recording or otherwise, without the
-* prior written permission of Gethin Taylor
-******************************************************************************/
-
-/******************************************************************************
-* \file  Form.cs   
-* \brief this file contains all function relating to the 
-* function of the game Gravy   
-******************************************************************************/
-
-/******************************************************************************
-* INCLUDE FILES
-******************************************************************************/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,45 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-/******************************************************************************
-* NAMESPACE AND CLASSESS
-******************************************************************************/
 namespace Minefield
 {
     public partial class Form1 : Form
     {
-
-/******************************************************************************
-* TYPEDEFS
-******************************************************************************/
-
-
-/******************************************************************************
-* #DEFINES and CONSTANTS
-******************************************************************************/
+        //start location of sprite
+        int atCol = 10;
+        int atRow = 19;
+        bool[,] bombs = new bool[20, 20];
 
 
-/******************************************************************************
-* GLOBAL VARIABLES
-******************************************************************************/
-//start location of sprite
-int atCol = 10;
-int atRow = 19;
-bool[,] bombs = new bool[20, 20];
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-
-/******************************************************************************
-* STATIC FUNCTION PROTOTYPES
-******************************************************************************/
-
-
-/******************************************************************************
-* LOCAL VARIABLES (static)
-******************************************************************************/
-
-/*
- * This function counts the number of bombs in the area of the sprite
- */
         private int bombCount(int atCol, int atRow)
         {
             int soFar = 0;
@@ -95,37 +55,36 @@ bool[,] bombs = new bool[20, 20];
                     soFar++;
                 }
             }
-            return soFar; //returns the value after incrementations.
+
+            return soFar;
         }
 
-/*
- * This function counts the number of bombs in the area of the sprite
- */
         private void ShowDaBomb()
         {
             Label lbl;
-            for (int atRow = 0; atRow < 20; atRow++)
+            for(int atRow = 0; atRow < 20; atRow++)
             {
                 for (int atCol = 0; atCol < 20; atCol++)
                 {
                     lbl = getLabel(atCol, atRow);
-                    if (bombs[atCol, atRow])
+                    if(bombs[atCol,atRow])
                     {
-                        lbl.Image = Properties.Resources.monster;
+                        lbl.BackColor = Color.Red;
+                    }
+                    else
+                    {
                         lbl.BackColor = Color.Black;
                     }
+
                 }
             }
 
         }
 
-/*
- * This function counts the number of bombs in the area of the sprite
- */
         private void PlaceBombs(int numBombs)
         {
             Random r = new Random(); //making random number
-                                     //creating the variable for the algirithm
+            //creating the variable for the algirithm
             int tryCol, tryRow;
             int setSofar = 0;
 
@@ -147,14 +106,11 @@ bool[,] bombs = new bool[20, 20];
             }
             catch
             {
-                MessageBox.Show("A dreaded Error has happend. This is a calculation issue, " +
-                    "i think.... please reset the game.");
+                MessageBox.Show("A dreaded Error has happend. This is a calculation issue, i think.... please reset the game.");
             }
         }
 
-/*
-* Function to return Label at (atCol, atRow)
-*/
+        //function to return Label at (atCol, atRowl)
         private Label getLabel(int atCol, int atRow)
         {
             int k = atRow * 20 + atCol + 1;
@@ -173,33 +129,24 @@ bool[,] bombs = new bool[20, 20];
             return null;
         }
 
-/*
-*function to show sprite at (atCol, atRow)
-*/
+        //function to show sprite at (atCol, atRow)
         private void showSpriteAt(int atCol, int atRow)
         {
-            Label lbl = getLabel(atCol, atRow);     //get label at (atCol,atRow)
-            lbl.BackColor = Color.Red;          //set backcolour to white
-            lbl.Image = Properties.Resources.ghost;     //set to show image 
+            Label lbl = getLabel(atCol, atRow); 	//get label at (atCol,atRow)
+            lbl.BackColor = Color.Red;    		//set backcolour to white
+            lbl.Image = Properties.Resources.ghost; 	//set to show image 
 
         }
 
-/*
-*function to Hide sprite at (atCol, atRow)
-*/
         private void HideSpriteImage(int atCol, int atRow)
         {
             Label lbl = getLabel(atCol, atRow); //gets the location of the sprite
             lbl.Image = null;                   // removes the image of the sprite
         }
 
-/*
-*function that returns a messagebox if you land on a bomb
-* or land on the grave
-*/
         private void amIDead(int atCol, int atRow)
         {
-            if (bombs[atCol, atRow])
+            if (bombs[atCol,atRow])
             {
                 this.BackColor = Color.Black;
                 btndown.Enabled = false;
@@ -211,16 +158,7 @@ bool[,] bombs = new bool[20, 20];
                 btnright.Enabled = false;
                 btnright.BackColor = Color.Red;
                 ShowDaBomb();
-                MessageBox.Show("Mwaha you are trapped on this earth for eternity!!");
-            }
-            else if (atCol == 9 && atRow == 0)
-            {
-                btndown.Enabled = false;
-                btnup.Enabled = false;
-                btnleft.Enabled = false;
-                btnright.Enabled = false;
-                ShowDaBomb();
-                MessageBox.Show("You are free to rest your soul!");
+                lblstatus.Text = "Mwahahaha you are bound to earth for eternity!!";
 
             }
             else
@@ -230,27 +168,11 @@ bool[,] bombs = new bool[20, 20];
 
         }
 
-
-/******************************************************************************
-* INITIALISATION OF GAME
-******************************************************************************/
-        public Form1()
-         {
-             InitializeComponent();
-         }
-
-
- /******************************************************************************
- * GAME FUNCTION
- ******************************************************************************/
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Bob is a result of taking life for granted, As he does every year" +
-                " on Halloween after the snooker accident, his restless soul travels the earth. " +
-                "Demons approaches Bob promising rest if he can make it to his grave. the sly Demon " +
-                "failed to mention that there will be more of them hiding in the dark.");
+            MessageBox.Show("Bob is a result of taking life for granted, As he does every year after the snooker accident   ");
             label10.Image = Properties.Resources.gravestone;
-            showSpriteAt(atCol, atRow); //set ghost at location
+            showSpriteAt(atCol, atRow); //set fish at location
             PlaceBombs(40); //planting the bombs
         }
 
@@ -298,14 +220,5 @@ bool[,] bombs = new bool[20, 20];
             }
         }
 
-        private void Timer1_Tick(object sender, EventArgs e)
-        {
-            
-        }
-
-
-        /******************************************************************************
-        * END
-        ******************************************************************************/
     }
 }
